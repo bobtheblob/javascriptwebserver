3d5ef2e7a10bf37f944e04f8ca3fd5cc0213b504
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-03-09 19:45:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express()
-app.get('/mytestapp', function(request, response) {
-	console.log("My get function works");
-    response.send('Hello World!');
+app.get('/rbx', function(request, response) {
+	response.send('Hello World!');
 });
 app.listen(process.env.PORT,function(){
 	console.log("App listening on port process.env.PORT");
