c5ae1ecfd4dbbe828c0ca584c1723928d6b9673a
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-03-09 19:33:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const app = require("express")();
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+const express = require('express');
+const app = express()
+app.get('/mytestapp', function(request, response) {
+	console.log("My get function works");
+    response.send('Hello World!');
+});
+app.listen(3000,function(){
+	console.log("App listening on port 3000");
 });
