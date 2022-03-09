062ac20649c7f905cb92f216a310d5e3c5527fb0
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-03-10 01:12:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var myJson = {
 var myJson2 = {
     key: "myvalue"
 };
+var myJson3 = {
+    key: "myvalue"
+};
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.get('/rbx', function(request, response) {
@@ -14,9 +17,16 @@ app.get('/rbx', function(request, response) {
 app.get('/rbxplrs', function(request, response) {
 	response.send(myJson2);
 });
+app.get('/chat', function(request, response) {
+	response.send(myJson3);
+});
 app.post('/rbxplrs', function(request, response) {
 	myJson2 = request.body;
 	response.send(myJson2);
+});
+app.post('/chat', function(request, response) {
+	myJson3 = request.body;
+	response.send(myJson3);
 });
 app.post('/rbx', function(request, response) {
 	myJson = request.body;
