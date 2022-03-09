cc304e9c14be9e5bd5ebd40afa36ea8efcc18677
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-03-09 19:38:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.get('/mytestapp', function(request, response) {
 	console.log("My get function works");
     response.send('Hello World!');
 });
-app.listen(3000,function(){
-	console.log("App listening on port 3000");
+app.listen(40379,function(){
+	console.log("App listening on port 40379");
 });
