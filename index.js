6ba7139898a719b43409d7ffa29cc0360b0ac1e8
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-03-10 00:44:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const app = express();
 var myJson = {
     key: "myvalue"
 };
+var myJson2 = {
+    key: "myvalue"
+};
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.get('/rbx', function(request, response) {
 	response.send(myJson);
 });
+app.get('/rbxplrs', function(request, response) {
+	response.send(myJson2);
+});
+app.post('/rbxplrs', function(request, response) {
+	myJson2 = request.body;
+});
 app.post('/rbx', function(request, response) {
+	if (request.body.Players ~= null ) {
+		myJson2 = request.body;
+		return
+	}
 	myJson = request.body
-	response.send(myJson);
 });
 app.listen(process.env.PORT,function(){
 	console.log("App listening on port process.env.PORT");
