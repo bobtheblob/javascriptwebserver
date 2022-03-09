772d7599b227d51e414b09a6210bbbc3d29d3d9a
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-03-09 19:55:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 app.get('/rbx', function(request, response) {
 	response.send('Hello World!');
 });
+app.post('/rbx', function(request, response) {
+	response.send('PUT request to homepage');
+});
 app.listen(process.env.PORT,function(){
 	console.log("App listening on port process.env.PORT");
 });
